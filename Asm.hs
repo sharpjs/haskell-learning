bea2772afb7f8ac4b4254b268bb70f52e17ff59a
Jeff Sharp <jeff.sharp@git>
2015-10-11 17:20:10 -0500

    Cleanup

--- Asm.hs
+++ Asm.hs
@@ -15,24 +15,20 @@ import Data.Text.Lazy           (Text)
 import Data.Text.Lazy.Builder   (Builder, fromLazyText, toLazyText)
 
 data AsmState = AsmState
-    { asm_out :: Builder  -- accumulated output
+    { asmOut :: Builder -- accumulated output
     }
 
-fmapOut :: (Builder -> Builder) -> AsmState -> AsmState
-fmapOut f s @ AsmState { asm_out =   b } =
-              AsmState { asm_out = f b }
+initState :: AsmState
+initState = AsmState { asmOut = "" }
 
 append :: Builder -> AsmState -> AsmState
-append b = fmapOut (<> b)
-
+append b AsmState { asmOut = o      }
+       = AsmState { asmOut = o <> b }
 
 type Asm a = State AsmState a
 
-initState :: AsmState
-initState = AsmState { asm_out = "" }
-
 assemble :: Asm a -> Text
-assemble asm = toLazyText . asm_out . execAsm asm $ initState
+assemble asm = toLazyText . asmOut . execAsm asm $ initState
 
 runAsm :: Asm a -> AsmState -> (a, AsmState)
 runAsm = runState
@@ -47,14 +43,17 @@ write :: Builder -> Asm ()
 write = modify . append
 
 section :: Text -> Asm ()
-section t = write $ ".section " <> (fromLazyText t) <> "\n"
+section t = write
+    $  ".section "
+    <> fromLazyText t
+    <> eol
 
 directive :: Text -> [Text] -> Asm ()
 directive op args = write
     $  indent
-    <> (fromLazyText op)
+    <> fromLazyText op
     <> " "
-    <> (mconcat $ fmap fromLazyText args)
+    <> mconcat (fromLazyText <$> args)
     <> eol
 
 indent :: Builder
