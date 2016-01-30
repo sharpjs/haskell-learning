da79735e14fc8873e1f044e65e4095315e73587c
Jeff Sharp <jeff.sharp@git>
2016-01-30 07:49:00 -0600

    Use types rather than type classes for ops, in prep for AsmOp.
    Type classes worked for ConOp, but will not work for AsmOp, due to lack
    of type-level information in some of AsmOp's members.

--- src/Aex/CodeGen/Ops.hs
+++ src/Aex/CodeGen/Ops.hs
@@ -49,54 +49,56 @@ instance ShowAsm a => ShowAsm (Operand a) where
 
 --------------------------------------------------------------------------------
 
-data Arity1 t a = A1 a
+data Arity1 a = A1 a
     deriving (Eq, Show, Functor, Foldable, Traversable)
 
-data Arity2 t a = A2 a a
+data Arity2 a = A2 a a
     deriving (Eq, Show, Functor, Foldable, Traversable)
 
-data Arity3 t a = A3 a a a
+data Arity3 a = A3 a a a
     deriving (Eq, Show, Functor, Foldable, Traversable)
 
-onA1 :: (a -> r) -> Arity1 t a -> r
+onA1 :: (a -> r) -> Arity1 a -> r
 onA1 f (A1 a) = f a
 
-onA2 :: (a -> a -> r) -> Arity2 t a -> r
+onA2 :: (a -> a -> r) -> Arity2 a -> r
 onA2 f (A2 a b) = f a b
 
-onA3 :: (a -> a -> a -> r) -> Arity3 t a -> r
+onA3 :: (a -> a -> a -> r) -> Arity3 a -> r
 onA3 f (A3 a b c) = f a b c
 
 --------------------------------------------------------------------------------
 
-class Traversable a => ConstOp a where
-    coTypeCheck :: a TypeA   -> Maybe TypeA
-    coEvalInt   :: a Integer -> Integer
-    coEvalFloat :: a Double  -> Double
-    coEvalExp   :: a Exp     -> Exp
+data Traversable a => ConOp a = ConOp
+    { conTypeCheck :: a TypeA   -> Maybe TypeA
+    , conEvalInt   :: a Integer -> Integer
+    , conEvalFloat :: a Double  -> Double
+    , conEvalExp   :: a Exp     -> Exp
+    }
 
-    coInvoke :: a (Operand Exp) -> Maybe (Operand Exp)
-    coInvoke os =
-        let es = dataOf <$> os
-            ts = typeOf <$> os
-            t  = coTypeCheck ts
-            n  = IntVal . coEvalInt <$> traverse intVal es
-            e' = coEvalExp es
-            e  = fromMaybe e' n
-        in Operand e <$> t
+conInvoke :: Traversable a => ConOp a -> a (Operand Exp) -> Maybe (Operand Exp)
+conInvoke op args =
+    let exps = dataOf <$> args
+        tys  = typeOf <$> args
+        ty   = conTypeCheck op tys
+        iv   = IntVal . conEvalInt op <$> traverse intVal exps
+        ev   = conEvalExp op exps
+        exp  = fromMaybe ev iv
+    in Operand exp <$> ty
 
-data AddConst
-data SubConst
+addC :: ConOp Arity2
+addC = ConOp
+    { conTypeCheck = onA2 checkTypesCompat
+    , conEvalInt   = onA2 (+)
+    , conEvalFloat = onA2 (+)
+    , conEvalExp   = onA2 (Add "")
+    }
 
-instance ConstOp (Arity2 AddConst) where
-    coTypeCheck = onA2 checkTypesCompat
-    coEvalInt   = onA2 (+)
-    coEvalFloat = onA2 (+)
-    coEvalExp   = onA2 (Add "")
-
-instance ConstOp (Arity2 SubConst) where
-    coTypeCheck = onA2 checkTypesCompat
-    coEvalInt   = onA2 (-)
-    coEvalFloat = onA2 (-)
-    coEvalExp   = onA2 (Sub "")
+subC :: ConOp Arity2
+subC = ConOp
+    { conTypeCheck = onA2 checkTypesCompat
+    , conEvalInt   = onA2 (-)
+    , conEvalFloat = onA2 (-)
+    , conEvalExp   = onA2 (Sub "")
+    }
 
