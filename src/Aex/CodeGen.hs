ee9998e493c4d00cf6d8975848ba69dbe50421d5
Jeff Sharp <jeff.sharp@git>
2016-02-03 07:21:26 -0600

    Stub CodeGen module.

---
+++ src/Aex/CodeGen.hs
@@ -0,0 +1,30 @@
+{-
+    Code Generation
+
+    This file is part of AEx.
+    Copyright (C) 2016 Jeffrey Sharp
+    
+    AEx is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published
+    by the Free Software Foundation, either version 3 of the License,
+    or (at your option) any later version.
+    
+    AEx is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+    the GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with AEx.  If not, see <http://www.gnu.org/licenses/>.
+-}
+
+module Aex.CodeGen where
+
+import Aex.Scope
+
+-- TODO
+data Context s = Context
+    { scope :: Scope s
+    --, out   :: Output
+    }
+
